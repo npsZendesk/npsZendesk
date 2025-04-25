581044b86a0bdbc5d99241d67923399e43e90bc6
Felipe Bicaletto <febicaletto@gmail.com>
2025-04-25 15:06:36 -0300

    Sistema de divisão de tipo de usuário

--- ProjetoNPS.py
+++ ProjetoNPS.py
@@ -1,21 +1,22 @@
 # Importar as bibliotecas necessárias
 import openpyxl
 from zenpy import Zenpy
+# CustomField é uma função para preencher campos de tickets do Zendesk
 from zenpy.lib.api_objects import CustomField
-
-# Fazer login no Zendesk
 from zenpy.lib.api_objects import Ticket, User
+ 
+# Fazer login no Zendesk
 creds = {
     'email': 'hellen.araujo@conexasaude.com.br',
     'token': 'rYjykv69LkLxcsLDOVpPkdHPCGZXLaG6NziGQ6rQ',
     'subdomain': 'conexasaude3465'
 }
-
+ 
 zenpy_client = Zenpy(**creds)
  
 # Abrir o arquivo Excel
-workbook = openpyxl.load_workbook('nps-zendesk-v1.xlsx')
-worksheet = workbook['nps']
+workbook = openpyxl.load_workbook('Teste Projeto NPS.xlsx')
+worksheet = workbook['Planilha1']
  
 # Percorrer as linhas da planilha
 for row in worksheet.iter_rows(min_row=2):
@@ -24,25 +25,25 @@ for row in worksheet.iter_rows(min_row=2):
     requester_email = row[1].value
     title = row[2].value
     description = row[3].value
-    
+    user_type = row[4].value
+   
     # Criar um ticket no Zendesk
     ticket_audit = zenpy_client.tickets.create(
         Ticket(
-            subject="Teste - npsZendesk",
-            description="Teste - npsZendesk",
+            subject= title,
+            description= description,
             requester=
                 User(
-                    name="Hellen Araujo",
-                    email="hellenaraujo703@gmail.com"
+                    name= requester_name,
+                    email= requester_email
                 ),
             custom_fields=[
-                #CustomField(
-                    #id=, value=)
+                CustomField(
+                    id=31483088708887, value= user_type)
             ]
         )
     )
  
     # Imprimir o número do ticket criado
-    print(f'Ticket {ticket_audit.id} criado.')
+    # print({ticket_audit})
     # Inserir ID do ticket na planilha.
- 
